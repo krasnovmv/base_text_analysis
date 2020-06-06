0fa58e7d22372034261c8165f0b420ba4d8f37e3
Краснов Михаил <famaup@mail.ru>
2020-06-07 02:35:38 +0300

    Добавлена возможность аналазировать сразу два файла

--- bag.py
+++ bag.py
@@ -14,18 +14,31 @@ from nltk.corpus import stopwords
 CORRECT_LEMMATIZE = False
 CHECK_STOPWORDS = False
 
-input_file_name = "Золотой ключик.txt"
+input_first_file_name = "Волшебник изумрудного города.txt"
+input_second_file_name = "Удивительный Волшебник Из Страны Оз.txt"
 
 
-def count_bag(sentences: List[str]) -> Tuple:
+def count_bag(
+    sentences_of_first_text: List[str], sentences_of_second_text: List[str]
+) -> Tuple:
     tokenizer = Tokenizer()
-    tokenizer.fit_on_texts(sentences)
-    sequences = tokenizer.texts_to_sequences(sentences)
-    flat_sequences = [item for sub_sequences in sequences for item in sub_sequences]
+    tokenizer.fit_on_texts(sentences_of_first_text + sentences_of_second_text)
+    sequences_of_first_text = tokenizer.texts_to_sequences(sentences_of_first_text)
+    sequences_of_second_text = tokenizer.texts_to_sequences(sentences_of_second_text)
+    flat_sequences_of_first_text = [
+        item for sub_sequences in sequences_of_first_text for item in sub_sequences
+    ]
+    flat_sequences_of_second_text = [
+        item for sub_sequences in sequences_of_second_text for item in sub_sequences
+    ]
     word_index = tokenizer.word_index
     bow = {}
     for key in word_index:
-        bow[key] = flat_sequences.count(word_index[key])
+        counters = [
+            flat_sequences_of_first_text.count(word_index[key]),
+            flat_sequences_of_second_text.count(word_index[key]),
+        ]
+        bow[key] = [sum(counters), *counters]
     return bow, word_index
 
 
@@ -55,8 +68,8 @@ def clear_sentence(text):
         tokens = mystem.lemmatize(text.strip().lower())
         tokens = filter(
             lambda token: token.strip()
-                          and not is_stopword(token)
-                          and not has_punctuation(token),
+            and not is_stopword(token)
+            and not has_punctuation(token),
             tokens,
         )
     else:
@@ -69,23 +82,33 @@ def clear_sentence(text):
 
 
 def get_output_file_name():
-    return f"{input_file_name.strip('.txt')}_{'lemmatized' if CORRECT_LEMMATIZE else 'not_lemmatized'}_{'without_stopwords' if CHECK_STOPWORDS else 'with_stopwords'}.json"
+    return f"{input_first_file_name.strip('.txt')}_{input_second_file_name.strip('.txt')}_{'lemmatized' if CORRECT_LEMMATIZE else 'not_lemmatized'}_{'without_stopwords' if CHECK_STOPWORDS else 'with_stopwords'}.json"
 
 
 def main():
-    input_file_path = pathlib.Path.cwd() / 'input' / input_file_name
-    with open(str(input_file_path), "r", encoding="UTF-8") as input_file:
-        raw_text = input_file.readlines()
-        text = [
+    input_first_file_path = pathlib.Path.cwd() / "input" / input_first_file_name
+    input_second_file_path = pathlib.Path.cwd() / "input" / input_second_file_name
+    with open(
+        str(input_first_file_path), "r", encoding="UTF-8"
+    ) as input_first_file, open(
+        str(input_second_file_path), "r", encoding="UTF-8"
+    ) as input_second_file:
+        first_raw_text = input_first_file.readlines()
+        second_raw_text = input_second_file.readlines()
+        first_text = [
             clear_sentence(sentence)
-            for sentence in tqdm(raw_text, unit="line", desc="prepare_sentences")
+            for sentence in tqdm(first_raw_text, unit="line", desc="prepare_sentences")
             if sentence.strip()
         ]
-        bag, word_index = count_bag(text)
-        print(bag)
-        print(f"We found {len(word_index)} unique tokens.")
+        second_text = [
+            clear_sentence(sentence)
+            for sentence in tqdm(second_raw_text, unit="line", desc="prepare_sentences")
+            if sentence.strip()
+        ]
+        bag, word_index = count_bag(first_text, second_text)
+        print(f"Найдено {len(word_index)} уникальных слов.")
         output_file_name = get_output_file_name()
-        output_file_path = pathlib.Path.cwd() / 'output' / output_file_name
+        output_file_path = pathlib.Path.cwd() / "output" / output_file_name
         output_file_path.parent.mkdir(exist_ok=True, parents=True)
         with open(str(output_file_path), "w+", encoding="UTF-8") as output_file:
             json.dump(bag, output_file, indent=4, ensure_ascii=False)
